98c7e40064ba359f8013f9454e36bcd56f7a08be
shareefmhd03 <80768690+shareefmhd03@users.noreply.github.com>
2021-06-03 01:54:23 +0530

    5thtry

--- Ticker/settings.py
+++ Ticker/settings.py
@@ -87,34 +87,26 @@ AUTH_USER_MODEL  = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-
-#     'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-#         'NAME': config('DB_NAME'),
-
-#         'USER': config('DB_USER'),
-
-#         'PASSWORD': config('DB_PASSWORD'),
-
-#         'HOST': config('DB_HOST'),
-
-#         'PORT': config('DB_PORT'),
-
-#     }
-
-# }
-
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': config('DB_NAME'),
+
+        'USER': config('DB_USER'),
+
+        'PASSWORD': config('DB_PASSWORD'),
+
+        'HOST': config('DB_HOST'),
+
+        'PORT': config('DB_PORT'),
+
+    }
+
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
