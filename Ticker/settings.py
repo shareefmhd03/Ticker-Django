50b0560d4b48b88c0ef9e21da5f1533ecd959827
shareefmhd03 <80768690+shareefmhd03@users.noreply.github.com>
2021-06-03 08:59:00 +0530

    8th try

--- Ticker/settings.py
+++ Ticker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast= bool)
 
-ALLOWED_HOSTS = ['ticker-test-dev3.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
